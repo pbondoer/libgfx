f2448c4aeaf94b0f292596d0c8da72c493e4e951
Pierre BONDOERFFER <pbondoer@student.42.fr>
2017-01-02 10:55:31 +0100

    add global make variables

--- gfx.mk
+++ gfx.mk
@@ -6,7 +6,7 @@
 #    By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/12/31 05:18:12 by pbondoer          #+#    #+#              #
-#    Updated: 2016/12/31 05:24:07 by pbondoer         ###   ########.fr        #
+#    Updated: 2017/01/02 10:55:19 by pbondoer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,3 +23,6 @@ GFX_NAME	= libgfx.a
 GFX_LNK		= -L $(L_GFX) -l gfx
 GFX_INC		= -I $(L_GFX)/includes
 GFX_LIB		= $(L_GFX)/$(GFX_NAME)
+
+LIB_LNK			+= $(GFX_LNK)
+LIB_INC			+= $(GFX_INC)
